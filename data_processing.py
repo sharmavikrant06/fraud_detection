ca64b3d8bf36393694ebbb0866ca37ae27582687
sharmavikrant06 <vikrantsharmamax@gmail.com>
2022-05-14 19:47:21 +0530

    adding data processing incorrect python code

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,11 @@
 
-
 print("Adding all the data related to fraud detection")
+
+a = 5
+b = 7
+
+def sum(a, b);
+    c= a+b
+    return a
+    
+print("Adding a new function sum which is not correct");
