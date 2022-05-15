897c8d56c66cbf2f26a5260a43c43f6232e7c955
sharmavikrant06 <vikrantsharmamax@gmail.com>
2022-05-15 10:41:47 +0530

    reverting the sum function code

--- data_processing.py
+++ data_processing.py
@@ -1,11 +1,3 @@
 
+
 print("Adding all the data related to fraud detection")
-
-a = 5
-b = 7
-
-def sum(a, b);
-    c= a+b
-    return a
-    
-print("Adding a new function sum which is not correct");
